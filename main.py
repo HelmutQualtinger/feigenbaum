04aca8444d45a422f213466f5f6b574b43e06d22
Helmut Qualtinger <94598493+HelmutQualtinger@users.noreply.github.com>
2021-12-19 18:46:10 +0100

    Yet another example of Chaos from simple rules

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def log_iterate(zn,r,n):
 converge=[]
 growth=[]
 cnt=0
-rs=np.arange(0.,4.,0.000001)
+rs=np.arange(2.9,4.,0.000001)
 converge = log_iterate(np.random.uniform(0.,1.,size=rs.shape),rs,100)
 print(cnt)
 plt.figure()
+plt.title("Convergence value versus Growth rate")
 plt.plot(rs, converge,'.',markersize=0.1)
 
 plt.show()
