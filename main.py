c983bb2d24c3f9efa92641a31b98f07e8140c2dc
Helmut Qualtinger <94598493+HelmutQualtinger@users.noreply.github.com>
2021-12-19 18:35:04 +0100

    Yet another example of Chaos from simple rules

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def log_iterate(zn,r,n):
+    for i in range(n):
+        zn=r*zn*(1-zn)
+    return zn
+
+converge=[]
+growth=[]
+cnt=0
+rs=np.arange(0.,4.,0.000001)
+converge = log_iterate(np.random.uniform(0.,1.,size=rs.shape),rs,100)
+print(cnt)
+plt.figure()
+plt.plot(rs, converge,'.',markersize=0.1)
+
+plt.show()
